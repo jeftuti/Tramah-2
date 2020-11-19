7d35074059830044c420233b4ec1ce1a50dca6d2
jeftuti <jefersonmb@gmail.com>
2020-11-19 17:40:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qj8zmj-yn1mhc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qj8zmj-yn1mhc)
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    name: "Parede",
+    price: 80,
+    description:
+      "São iténs que vão alegrar seus ambientes desde uma parede da sala até a parede de seu quarto todos com muita elegancia e bom gosto"
+  },
+  {
+    name: "Natal",
+    price: 50,
+    description:
+      "Um dos momentos mais esperados do ano é deixar a alegria do natal tomar conta de seus ambientes enfeites natalinos de todos os tamanhos em Macramê"
+  },
+  {
+    name: "Espelhos",
+    price: 40,
+    description:
+      "Como se ver e não ver a sua beleza através de um belissimo item , combinamos do tamanho que quizer se ver"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
